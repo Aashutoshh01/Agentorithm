1e8a8bbf22b33264692acd23ece6ca5bffed00f4
Aashutosh Joshi <136259002+Aashutoshh01@users.noreply.github.com>
2025-08-18 02:26:22 +0530

    Add files via upload

---
+++ config/docker_utils.py
@@ -0,0 +1,8 @@
+async def start_docker_container(docker):
+    print("Starting Docker Container.")
+    await docker.start()
+
+async def stop_docker_container(docker):
+    print("Stopping Docker Container.")
+    await docker.stop()
+    print("Docker Container Stopped.")
